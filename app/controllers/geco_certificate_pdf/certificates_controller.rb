10d7155a7776c84290a803b997fdd8b9582f2664
Jervey <jerveybenitez@gmail.com>
2026-07-08 16:47:22 +0800

    removed the underline for styling

--- app/controllers/geco_certificate_pdf/certificates_controller.rb
+++ app/controllers/geco_certificate_pdf/certificates_controller.rb
@@ -67,7 +67,7 @@ module GecoCertificatePdf
         # student name
         pdf.font("Radley", style: :italic) do 
           pdf.formatted_text_box(
-            [{ text: user.name, styles: [:underline], size: 36 }],
+            [{ text: user.name, size: 36 }],
             at: [0, 310], width: 842, align: :center
           ) 
         end
@@ -78,7 +78,7 @@ module GecoCertificatePdf
           pdf.formatted_text_box(
             [
               { text: "Successful completion of an ", color: blue, size: 14 },
-              { text: "instructor-led upskilling program", color: blue, size: 14, styles: [:underline] },
+              { text: "instructor-led upskilling program", color: blue, size: 14 },
               { text: " titled,", color: blue, size: 14 }
             ],
             at: [0, 237], width: 842, align: :center
@@ -86,11 +86,11 @@ module GecoCertificatePdf
 
           pdf.formatted_text_box(
             [
-              { text: course.name, color: blue, size: 14, styles: [:underline] },
+              { text: course.name, color: blue, size: 14 },
               { text: " conducted on ", color: blue, size: 14 },
-              { text: start_date, color: blue, size: 14, styles: [:underline] },
+              { text: start_date, color: blue, size: 14 },
               { text: " to ", color: blue, size: 14 },
-              { text: finish_date, color: blue, size: 14, styles: [:underline] },
+              { text: finish_date, color: blue, size: 14 },
               { text: ".",  color: blue, size: 14 }
             ],
             at: [0, 220], width: 842, align: :center
